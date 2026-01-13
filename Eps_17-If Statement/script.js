10d4a360654361ba10e14da4424e349d09513bd8
Marisa Al Zahra <zica79637@gmail.com>
2026-01-13 10:48:36 +0700

    Create script.js

---
+++ Eps_17-If Statement/script.js
@@ -0,0 +1,11 @@
+let nama = "chirroww";
+
+console.log("nama" + "=" + nama);
+
+if (nama==="chirroww"){
+    console.log("Hallooo chirroww");
+}else{
+    console.log("bukan chirrow , wow srepet");
+}
+
+console.log("akhir dari program");
