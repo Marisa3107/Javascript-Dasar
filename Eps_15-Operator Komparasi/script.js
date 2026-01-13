efc6befc34690d78b549a8f4643a4653a0da1f15
Marisa Al Zahra <zica79637@gmail.com>
2026-01-13 10:44:31 +0700

    Create script.js

---
+++ Eps_15-Operator Komparasi/script.js
@@ -0,0 +1,30 @@
+// operator komparasi
+// == equality
+// != inequality
+// === strict equality
+// !== strict inequality
+// > lebih besar dari
+// >= lebih besar sama dengan
+// < lebih kecil
+// <= lebih kecil sama dengan
+
+console.log(`15 > 7 = ${15 > 7}`);
+let dataBool = 15 > 7;
+console.log(`15 > 7 = ${dataBool}`);
+
+console.log(`12 >= 12 = ${12 >= 12}`);
+console.log(`7 < 15 = ${7 < 15}`);
+console.log(`7 <= 7 = ${7 <= 7}`);
+
+//persamaan
+console.log(`3 == "3" = ${3 == "3"}`);
+console.log(`3 != 3 = ${3 != "3"}`);
+
+console.log(`3 === 3 = ${3 === 3}`);
+console.log(`3 === "3" = ${3 === "3"}`);
+console.log(`"3" === "3" = ${"3" === "3"}`);
+
+console.log(`3 !== 3 = ${3 !== 3}`);
+console.log(`3 !== "3" = ${3 !== "3"}`);
+console.log(`"3" !== "3" = ${"3" !== "3"}`);
+
