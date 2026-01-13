bd2f1af85b34c9f722aa1564d20bda5e9c9c007a
Marisa Al Zahra <zica79637@gmail.com>
2026-01-13 10:30:52 +0700

    update
    Updated script.js to include examples of NaN and Undefined.

---
+++ Eps 11_NaN dan Undefined/script.js
@@ -0,0 +1,17 @@
+// NaN dan Undefined
+
+// NaN = Not a Number
+console.log(`akar dari -1 :${Math.sqrt(-1)}`);
+console.log(`"Chirrow / "Chaa :${"Chirrow" / "Chaa"}`);
+console.log(`"Chirrow * "Chaa :${"Chirrow" * "Chaa"}`);
+console.log(`"Chirrow + "Chaa :${"Chirrow" + "Chaa"}`);
+console.log(`"Chirrow - "Chaa :${"Chirrow" - "Chaa"}`);
+
+let data = parseInt("test123");
+console.log(data + 5); //NaN tidak bisa di operasikan lagi
+
+// Undefined - Belum di ketahui nilai nya
+let a;
+console.log(`a = ${a}`);
+console.log(Math.sqrt(4));
+console.log(console.log("test"));
