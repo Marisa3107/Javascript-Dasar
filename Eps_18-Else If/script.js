b80826c101014c3542a43e633e611ac4851d44fa
Marisa Al Zahra <zica79637@gmail.com>
2026-01-13 10:50:04 +0700

    Create script.js

---
+++ Eps_18-Else If/script.js
@@ -0,0 +1,23 @@
+console.log("mulai");
+
+let nama = "chirroww";
+
+if (nama === "xenn"){
+    console.log("hai xenn kereenn");
+} else if(nama === "chirroww"){
+    console.log("hwellow chirroww");
+} else {
+    console.log("Tidak Di Kenal!");
+}
+
+switch(nama){
+    case "xenn":
+        console.log("switch case xenn");
+        break;
+    case "chirroww":
+        console.log("switch case chirroww");
+        break;
+    default:
+        console.log("switch case nobody");
+        
+}
