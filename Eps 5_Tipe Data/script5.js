06ca8fba8f9927e5cc0615d60f32344a2ed53cfd
Marisa Al Zahra <zica79637@gmail.com>
2026-01-13 10:03:33 +0700

    Create script5.js
    Add tutorial on data types in JavaScript including string, number, boolean, and undefined.

---
+++ Eps 5_Tipe Data/script5.js
@@ -0,0 +1,24 @@
+//tutorial tentang tipe data
+
+let dataString = "chirroww"; //string
+console.log(dataString);
+console.log(typeof dataString)
+
+let dataNumber = 10.760; //number
+console.log(dataNumber);
+console.log(typeof dataNumber);
+
+let dataBoolean = false; //boolean
+console.log(dataBoolean);
+console.log(typeof dataBoolean);
+
+dataString = 2;
+console.log(dataString);
+console.log(typeof dataString)
+
+let dataKosong; // underfined
+console.log(dataKosong);
+console.log(typeof dataKosong);
+dataKosong = "xenn chirrow";
+console.log(dataKosong);
+console.log(typeof dataKosong);
